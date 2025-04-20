590e2dc27f817f61a9e939979b73168ef40de7b0
OzMaatuk <ozmaatuk@gmail.com>
2025-04-20 18:21:43 +0000

    change browser

--- driver.py
+++ driver.py
@@ -2,8 +2,9 @@ from playwright.sync_api import sync_playwright, BrowserContext
 
 def initialize_driver(headless: bool = True, user_data_dir: str = None) -> BrowserContext:
     playwright = sync_playwright().start()
-    browser = playwright.webkit.launch_persistent_context(
+    browser = playwright.chromium.launch_persistent_context(
                 user_data_dir=user_data_dir,
                 headless=headless,
-                ignore_https_errors=True)
+                ignore_https_errors=True,
+                channel="msedge")
     return browser
